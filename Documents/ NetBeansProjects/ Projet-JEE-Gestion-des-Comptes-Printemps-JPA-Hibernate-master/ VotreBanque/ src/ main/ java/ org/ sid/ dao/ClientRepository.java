2c231de7b55ab8751ee9494272c4c913b4e2d82a
habibatou <44176693+habibatou@users.noreply.github.com>
2018-11-09 13:08:50 +0000

    Update and rename Documents/NetBeansProjects/Projet-JEE-Gestion-des-Comptes-Spring-JPA-Hibernate-master/VotreBanque/src/main/java/org/sid/dao/ClientRepository.java to  Documents/ NetBeansProjects/ Projet-JEE-Gestion-des-Comptes-Printemps-JPA-Hibernate-master/ VotreBanque/ src/ main/ java/ org/ sid/ dao/ClientRepository.java

--- Documents/ NetBeansProjects/ Projet-JEE-Gestion-des-Comptes-Printemps-JPA-Hibernate-master/ VotreBanque/ src/ main/ java/ org/ sid/ dao/ClientRepository.java
+++ Documents/ NetBeansProjects/ Projet-JEE-Gestion-des-Comptes-Printemps-JPA-Hibernate-master/ VotreBanque/ src/ main/ java/ org/ sid/ dao/ClientRepository.java
@@ -2,7 +2,7 @@ package org.sid.dao;
 
 import org.sid.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+::HABIBATOU
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
